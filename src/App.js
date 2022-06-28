50ee67e247427567946fe50f6638424cd5ea1834
Sai Chinmay <47316906+saichinmay2000@users.noreply.github.com>
2022-06-28 21:39:29 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amazon Clone</h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADSBDnys88akvpYfL7-Tb-LKMmtHRorEE",
+    authDomain: "clone-18b23.firebaseapp.com",
+    projectId: "clone-18b23",
+    databaseURL: "https://clone-18b23-default-rtdb.firebaseio.com",
+    storageBucket: "clone-18b23.appspot.com",
+    messagingSenderId: "198295765479",
+    appId: "1:198295765479:web:1c81a294b177f9950efd42",
+    measurementId: "G-R34WSE3NGN"
+  };
