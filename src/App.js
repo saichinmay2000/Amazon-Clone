e7a78baa5cfb7fe0f74e3f4dea4e35da61d4a264
Sai Chinmay <47316906+saichinmay2000@users.noreply.github.com>
2022-06-28 22:35:31 +0530

    Added Amazon Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Amazon Clone</h1>
-      {/* Header */}
+      {/* <h1>Amazon Clone</h1> */}
+      <Header/>
       {/* Home */}
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
     <div className='header'>
-        
+        <img
+          className='header_logo'
+          src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        />
+        <div
+        className='header_search'>
+            <input 
+            className='header_searchinput'
+            type="text"
+            />
+            <SearchIcon className='header_searchicon'/>
+        </div>
+
+        <div className='header_nav'>
+
+          <div className='header_option'>
+              <span className='header_optionLineOne'>Hello, Sign in</span>
+              <span className='header_optionLineTwo'>Accounts & Lists</span>
+          </div>
+
+          <div className='header_option'>
+              <span className='header_optionLineOne'>Returns</span>
+              <span className='header_optionLineTwo'>& Orders</span>
+          </div>
+
+          <div className='header_option'>
+              <span className='header_optionLineOne'>Your</span>
+              <span className='header_optionLineTwo'>Prime</span>
+          </div>
+
+          <div className='header_optionBasket'>
+                <ShoppingBasketIcon className='header_basket'/>
+                <span className='header_optionLineTwo header_basketCount'>0</span>
+          </div>
+        </div>
+
     </div>
   )
 }
